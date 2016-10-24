98e4ac3623181aee3d5768ed59a5a1caee1c9b7a
senritsu <teh.b4k4@gmail.com>
2016-10-24 08:29:00 +0200

    implemented simple proxy

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const rp = require('request-promise')
+const koa = require('koa')
+const route = require('koa-route')
+const app = koa()
+
+app.use(route.get('/:user/:project', badge))
+
+function *badge(user, project) {
+    const detailedCount = this.query.count
+    const buildStatus = yield rp(`https://ci.appveyor.com/api/projects/${user}/${project}`, { json: true })
+    const job = buildStatus.build.jobs[0]
+
+    const passed = job.passedTestsCount === job.testsCount
+    const color = passed ? 'brightgreen' : 'red'
+
+    if (detailedCount) {
+        const status = `${job.passedTestsCount}/${job.testsCount}`
+        this.redirect(`https://img.shields.io/badge/tests-${status}-${color}.svg`)
+    } else {
+        const status = passed ? 'passed' : 'failed'
+        this.redirect(`https://img.shields.io/badge/tests-${status}-${color}.svg`)
+    }
+}
+
+app.listen(3002)
